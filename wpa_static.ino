039d28b6485eaed8b7c4ba1790ba3091f991566a
johnmrushing <johnmrushing@gmail.com>
2019-03-26 21:04:18 -0400

    Add wpa_static.ino
    This is largely example code from the WiFi101 library

---
+++ wpa_static.ino
@@ -0,0 +1,84 @@
+#include <SPI.h>
+#include <WiFi101.h>
+
+#include "arduino_secrets.h" 
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;        // your network password (use for WPA, or use as key for WEP)
+
+int status = WL_IDLE_STATUS;     // the WiFi radio's status
+
+IPAddress ip(192, 168, 86, 225); 
+void setup() {
+  
+  WiFi.setPins(8,7,4,2);
+
+  WiFi.config(ip);
+  
+  Serial.begin(9600);
+  while (!Serial) {
+  }
+
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+
+    status = WiFi.begin(ssid, pass);
+
+    delay(10000);
+  }
+
+  Serial.print("You're connected to the network");
+  printCurrentNet();
+  printWiFiData();
+
+}
+
+void loop() {
+  delay(10000);
+  printCurrentNet();
+}
+
+void printWiFiData() {
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  Serial.println(ip);
+
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  printMacAddress(mac);
+
+}
+
+void printCurrentNet() {
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  byte bssid[6];
+  WiFi.BSSID(bssid);
+  Serial.print("BSSID: ");
+  printMacAddress(bssid);
+
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.println(rssi);
+
+  byte encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type:");
+  Serial.println(encryption, HEX);
+  Serial.println();
+}
+
+void printMacAddress(byte mac[]) {
+  for (int i = 5; i >= 0; i--) {
+    if (mac[i] < 16) {
+      Serial.print("0");
+    }
+    Serial.print(mac[i], HEX);
+    if (i > 0) {
+      Serial.print(":");
+    }
+  }
+  Serial.println();
+}
